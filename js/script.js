71980e2e49459d7f09aa5a60b0622de6fc22b32a
kevin joel noviello <kjnoviello@gmail.com>
2023-05-16 00:03:02 -0300

    dom aplicado a la tabla

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ let nombre;
 let cantidad; 
 let precio; 
 let total = cantidad*precio;
-const array = [];
+// const array = [];
 
 
 // DEFINO LA CLASE PARA ENTRADA DEL PRODUCTO CON EL PRECIO ACUMULATIVO A LA TABLA
@@ -50,17 +50,18 @@ class ListaEntradas {
                     precio = parseFloat(prompt("ingrese el precio del producto"));
                     this.totalAcumulado = this.totalAcumulado + cantidad*precio
                     
-                    const array = new Entrada(nombre, cantidad, precio);
-                    console.log(array);         // prueba de control
+                    const nuevaEntrada = new Entrada(nombre, cantidad, precio);
+                    this.listaEntrada.push(nuevaEntrada);
+                    console.log(this.listaEntrada);         // prueba de control
                     
                     let contenedor = document.getElementById("tablas");
                     
                     let nuevo = document.createElement("tr");
                     
                     nuevo.innerHTML = `
-                    <td>${array.nombre}</td>
-                    <td>${array.cantidad}</td>
-                    <td>$ ${array.precio}</td>
+                    <td>${nuevaEntrada.nombre}</td>
+                    <td>${nuevaEntrada.cantidad}</td>
+                    <td>$ ${nuevaEntrada.precio}</td>
                     <td>$ ${this.totalAcumulado}</td>
                     `;
     
@@ -68,61 +69,27 @@ class ListaEntradas {
                 };
             } while (nombre !== "ESC");
         };
+
+        // DEFINO LA FUNCION PARA BUSCAR UN PRODUCTO SEGUN EL NOMBRE
+        buscarListaEntrada(nombre){
+            const buscarProducto = this.listaEntrada.find(item => item.nombre === nombre);
+            if (buscarProducto) {
+               return (buscarProducto);
+            } else {
+               return (`No existe el producto ${nombre}`);
+            }
+        };
 };
 
 // EJECUTO LA FUNCION PARA AGREGAR PRODUCTOS A LA LISTA
 const productoAgregado = new ListaEntradas;
 const productoEnTabla = productoAgregado.agregarListaEntrada()
 console.log(productoEnTabla);
-console.log(productoAgregado.listaEntrada);
 
 
-
-
-
-
-
-// FUNCION QUE BUSCA UN PRODUCTO EN LA TABLA
-
-
-
-// Función que busca un producto por su sku en "la base de datos"
-/* function findProductByName(nombre) {
-        setTimeout(() => {
-            const foundProduct = productosDelSuper.find(product => product.nombre === nombre);
-            if (foundProduct) {
-                (foundProduct);
-            } else {
-                (`No existe el producto ${nombre}`);
-            }
-        }, 1500);
-    }; */
-
-
-// function agregarEntrada(nombre, cantidad, precio) {
-//     console.log(`Agregando ${nombre} ${cantidad} unidad/es a ${precio} pesos`);
-
-//         // Busco el producto en la "base de datos"
-//         const entrada = findProductByNombre(nombre);
-//         console.log(`El producto agregado es ${entrada.nombre}`);
-        
-//         // me fijo si el producto ya existe
-//         const existeProducto = this.entrada.find(item => item.nombre === nombre);
-        
-//         // si existe solo le sumo la cantidad
-//         if (existeProducto) {
-//             existeProducto.cantidad += cantidad;
-
-//         // si no existe lo agrego y tambien verifico si existe la misma categoria a agregar.
-//         } else {
-//             const nuevoProducto = new ProductoEnCarrito(sku, producto.nombre, cantidad);
-//             this.productos.push(nuevoProducto);
-//             if (!this.categorias.includes(producto.categoria)) {
-//                 this.categorias.push(producto.categoria);}
-//         };
-
-//         // se modifica el precio
-//         this.precioTotal = this.precioTotal + (producto.precio * cantidad);
-//         console.log(`el carrito queda conformado por los productos agregados`, carrito.productos);
-//         console.log(`El precio total del carrito al agregar el producto es de ${this.precioTotal}`);
-// };
+// EJECUTO LA FUNCION PARA BUSCAR UN PRODUCTO EN LA TABLA
+// SE LE AGREGA EL TIME OUT PARA ESPERAR A CARGAR EL PRODUCTO
+setTimeout(() => {
+    const nuevoBuscar = productoAgregado.buscarListaEntrada("arroz")
+    console.log(nuevoBuscar);
+}, 10000);
