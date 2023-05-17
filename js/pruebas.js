7aed090c958fd6bff49a77590b3fe946f12510e0
kevin joel noviello <kjnoviello@gmail.com>
2023-05-17 13:21:27 -0300

    dark mode

--- js/pruebas.js
+++ js/pruebas.js
@@ -2,5 +2,26 @@
 // let title = document.getElementById("titulo");
 // title.innerText = "LA LISTA DEL SUPER"
 
-// let color = document.getElementById("bodyColor")
-// color.className = "bodyColor"
+
+// light mode
+let boton = document.getElementById("btn");
+let body = document.getElementById("bodyColor")
+
+function lightMode() {
+    console.log("antes del clik");
+    body.className = "bodyColor";
+    console.log("despues del click");
+};
+
+boton.onclick = lightMode;
+
+// dark mode
+let botondark = document.getElementById("btn-dark");
+let bodyDark = document.getElementById("bodyColor")
+function darkMode() {
+    console.log("antes del clik dark mode");
+    bodyDark.className = "body";
+    console.log("despues del click dark mode");
+}
+
+botondark.onclick = darkMode;
